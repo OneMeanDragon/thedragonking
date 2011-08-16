48017a5c2c3f2354ca7ba737ffd8bfbd3eff2656
DragonKings <dragonkings@gmail.com>
2011-08-16 03:43:28 +0000

    Initial upload

---
+++ Botnet/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Threading
+Imports System.Net.Sockets
+Imports System.Xml.Serialization
+Imports System.IO
+Imports System.Net
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        lbMemory.Text = "Used memory: " & Format(GC.GetTotalMemory(True), "### ### ##0 bytes")
+        lbLoadtime.Text = "Server started at: " & Now
+        StartServer()
+        'Debug.Print(GetFlagCharectersFromFlag(AddFlagsToUser(0, "abcdefghijklmnopqrstuvwxyz")))
+        'Debug.Print(GetFlagCharectersFromFlag(RemoveFlagsFromUser(AddFlagsToUser(0, "abcdefghijklmnopqrstuvwxyz"), "acegikmoqsuwy")))
+    End Sub
+
+    <System.MTAThreadAttribute()> _
+    Sub StartServer()
+        timeBeginPeriod(1)  'Set timeGetTime to a accuracy of 1ms
+
+        Dim dateTimeStarted As Date = Now
+        Debug.Print("INFORMATION, [" & Format(TimeOfDay, "hh:mm:ss") & "] Server Starting...")
+
+        ApplicationsPath = Application.StartupPath & "\"
+
+        Dim currentDomain As AppDomain = AppDomain.CurrentDomain
+        AddHandler currentDomain.UnhandledException, AddressOf GenericExceptionHandler
+
+        WS = New WorldServerClass
+        GC.Collect()
+
+        Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High
+    End Sub
+
+    Private Sub GenericExceptionHandler(ByVal sender As Object, ByVal e As UnhandledExceptionEventArgs)
+        Dim EX As Exception
+        EX = e.ExceptionObject
+
+        Debug.Print("CRITICAL, " & EX.ToString)
+        Debug.Print("FAILED, Unexpected error has occured. An 'Error-yyyy-mmm-d-h-mm.log' file has been created.")
+
+        Dim tw As TextWriter
+        tw = New StreamWriter(New FileStream(String.Format("Error-{0}.log", Format(Now, "yyyy-MMM-d-H-mm")), FileMode.Create))
+        tw.Write(EX.ToString)
+        tw.Close()
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        lbMemory.Text = "Used memory: " & Format(GC.GetTotalMemory(True), "### ### ##0 bytes")
+    End Sub
+End Class
---
+++ Botnet/Network.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Threading
+Imports System.Reflection
+Imports System.Text.RegularExpressions
+Imports System.Net.Sockets
+Imports System.Xml.Serialization
+Imports System.IO
+Imports System.Net
+Imports System.Runtime.Remoting
+Imports System.Runtime.CompilerServices
+Imports System.Collections.Generic
+Imports System.Security.Permissions
+
+Public Module Network
+    Public WS As WorldServerClass
+
+    Class WorldServerClass
+        Inherits MarshalByRefObject
+        Implements IDisposable
+
+        <CLSCompliant(False)> _
+        Public m_flagStopListen As Boolean = False
+        Private m_TimerPing As Timer
+        Private m_TimerStats As Timer
+        Private m_TimerCPU As Timer
+
+        Private m_Socket As Socket
+        Public Sub New()
+            Try
+                'AddressFamily.InterNetwork
+                m_Socket = New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+                m_Socket.Bind(New IPEndPoint(IPAddress.Any, 21845))
+                m_Socket.Listen(5)
+                m_Socket.BeginAccept(AddressOf AcceptConnection, Nothing)
+                Debug.Print("LogType.SUCCESS Listening on " & IPAddress.Any.ToString & " on port " & 21845)
+
+            Catch e As Exception
+                Console.WriteLine()
+                Debug.Print("LogType.FAILED, Error in " & e.Message & ": " & e.Source & ".", e.Message, e.Source)
+            End Try
+        End Sub
+        Protected Sub AcceptConnection(ByVal ar As IAsyncResult)
+            If m_flagStopListen Then Return
+
+            Dim m_Client As New ClientClass
+            m_Client.Socket = m_Socket.EndAccept(ar)
+            'Thread.Sleep(100)
+            'MessageBox.Show((m_Client.Socket Is m_Socket).ToString())
+            m_Client.Socket.NoDelay = True
+            m_Client.Socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, 1)
+
+            m_Socket.BeginAccept(AddressOf AcceptConnection, Nothing)
+
+            ThreadPool.QueueUserWorkItem(New System.Threading.WaitCallback(AddressOf m_Client.OnConnect))
+        End Sub
+
+
+
+        Public Sub Dispose() Implements IDisposable.Dispose
+            m_flagStopListen = True
+            m_Socket.Close()
+        End Sub
+        <SecurityPermissionAttribute(SecurityAction.Demand, Flags:=SecurityPermissionFlag.Infrastructure)> _
+        Public Overrides Function InitializeLifetimeService() As Object
+            Return Nothing
+        End Function
+    End Class
+
+End Module
